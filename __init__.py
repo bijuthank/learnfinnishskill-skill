87a7b555c5d7361f9cb07e8a5da7f2dd77c03df9
Biju Thankachan <bijuthank@users.noreply.github.com>
2020-03-27 16:13:44 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,33 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.audio import wait_while_speaking, play_mp3
+import os
 
 
 class LearnFinnishskill(MycroftSkill):
+
+    path_tranlated_file = "/tmp/finnish.mp3"
+
+    def translated_to_finnish(self, text):
+        translated = translate(text, "fi")
+        self.speak_finnish(translated)
+
+    def speak_finnish(self, sentence):
+        wait_while_speaking()
+        get_sentence = 'wget -q -U Mozilla -O ' + self.path_translated_file + \
+                       '"https://translate.google.com/translate_tts?ie=UTF-8&tl=pt&q=' + \
+                       str(sentence) + '&client=tw-ob' + '"'
+
+        os.system(get_sentence)
+        play_mp3(self.path_tranlated_file)
+
     def __init__(self):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('learnFinnishskill.intent')
     def handle_learnFinnishSkill(self, message):
-        self.speak_dialog('learnfinnishskill')
+        # wait = True makes Mycroft wait until speech is finished playing before continuing
+        self.speak_dialog('learnfinnishskill', wait=True)
+        self.speak_finnish("moi")
 
 
 def create_skill():
